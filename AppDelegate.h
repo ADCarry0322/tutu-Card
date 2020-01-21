eccc757865421abe83ef3ff7456a098527050c5c
ADCarry0322 <55800461+ADCarry0322@users.noreply.github.com>
2020-01-21 19:58:16 +0800

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Card
+//
+//  Created by ios group on 2019/11/12.
+//  Copyright © 2019年 熊宁畅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
