a6564e12057b704e2e8ade780db1bcb44ef38347
ADCarry0322 <55800461+ADCarry0322@users.noreply.github.com>
2020-01-21 19:57:34 +0800

    Create  S.h

---
+++ S.h
@@ -0,0 +1,20 @@
+//
+//  S.h
+//  Card
+//
+//  Created by ios group on 2019/11/15.
+//  Copyright © 2019年 熊宁畅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface S : UIViewController
+
+@property(nonatomic) UITableView * tableView;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
